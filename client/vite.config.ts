cd35f4f27b3e00b8f2c62da2ba6c3908d2810c46
Eliyahu Levi <156104966+eliyahuOfficial@users.noreply.github.com>
2024-08-17 23:46:57 +0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/fetch-metadata': 'https://url-metadata-fetcher-server.onrender.com',
-    },
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://url-metadata-fetcher-server.onrender.com'),
   },
 });
