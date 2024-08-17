d6abe13f689686d54061b266956510f23e38db14
Eliyahu Levi <156104966+eliyahuOfficial@users.noreply.github.com>
2024-08-17 23:32:06 +0300

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: "*" }))
 
 const limiter = rateLimit({
     windowMs: 1000,
